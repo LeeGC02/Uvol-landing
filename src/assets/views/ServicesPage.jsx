54f18296c06cf3835566f2139e8a1a60813a9783
ByJox <joxromaylp@gmail.com>
2024-08-12 00:49:07 -0400

    Inicio terminado, header modificado, servicios terminados

--- src/assets/views/ServicesPage.jsx
+++ src/assets/views/ServicesPage.jsx
@@ -1,80 +1,73 @@
-import React from 'react'
+import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './ServicesPage.css';
-import HeaderMaster from '../components/HeaderMaster';
 import Footer from '../components/Footer';
+
+const services = [
+  {
+    title: "Desarrollo de Plataformas Digitales",
+    description: "Transformamos ideas en aplicaciones y plataformas digitales robustas y escalables.",
+    details: "Desde la conceptualización hasta el lanzamiento, nuestro equipo de desarrolladores y diseñadores trabaja en estrecha colaboración para crear soluciones que optimicen procesos y mejoren la experiencia del usuario. Aplicaciones móviles y plataformas web.",
+    image: "/App.jpg"
+  },
+  {
+    title: "Ciberseguridad",
+    description: "Servicios avanzados de ciberseguridad para proteger los datos y sistemas críticos de su organización.",
+    details: "Identificamos y corregimos puntos débiles en sus sistemas antes de que puedan ser explotados. Monitoreo y gestión de amenazas en tiempo real.",
+    image: "/Vulnerabildiad.webp"
+  },
+  {
+    title: "Integración de Sistemas",
+    description: "Facilitamos la integración de sistemas y aplicaciones para que su empresa funcione como una máquina bien engrasada.",
+    details: "Nuestros servicios de integración garantizan que todas sus herramientas y plataformas trabajen en conjunto de manera eficiente. Conectamos sistemas de gestión de clientes y recursos empresariales para mejorar la eficiencia operativa y automatizamos tareas repetitivas.",
+    image: "/erp-crm.webp"
+  },
+  {
+    title: "Consultoría Tecnológica",
+    description: "Asesoramiento estratégico para ayudar a su empresa a aprovechar las tecnologías emergentes y mantenerse competitiva en el mercado.",
+    details: "Evaluamos sus necesidades tecnológicas y diseñamos soluciones a medida que se alineen con sus objetivos comerciales. Ayudamos a adoptar nuevas tecnologías y rediseñar procesos para adaptarse al entorno digital actual.",
+    image: "/Consultoría tecnológica.webp"
+  },
+  {
+    title: "Soporte y Mantenimiento",
+    description: "Soporte técnico y mantenimiento continuo para asegurar que sus sistemas operen sin problemas y estén siempre actualizados.",
+    details: "Nuestro equipo está disponible en todo momento para resolver cualquier problema técnico que pueda surgir. Nos aseguramos de que sus sistemas estén siempre en la última versión y funcionando de manera óptima.",
+    image: "/soporte.gif"
+  },
+];
+
 const ServicesPage = () => {
   return (
     <div className='a'>
+      <div className='servicios-container'>
+        <h1 className="servicios-title">Nuestros Servicios</h1>
+        <p className="servicios-description">
+          En UVOL, estamos comprometidos con la creación de soluciones tecnológicas innovadoras que ayudan a nuestros clientes a transformar sus negocios y adaptarse a un mundo digital en constante evolución. Ofrecemos una gama de servicios diseñados para satisfacer las necesidades únicas de cada organización.
+        </p>
 
-      <div className="servicios-container">
-      <h1 className="servicios-title">Nuestros Servicios</h1>
-      <p className="servicios-description">
-        En UVOL, estamos comprometidos con la creación de soluciones tecnológicas innovadoras que ayudan a nuestros clientes a transformar sus negocios y adaptarse a un mundo digital en constante evolución. Ofrecemos una gama de servicios diseñados para satisfacer las necesidades únicas de cada organización.
-      </p>
-
-      <div className="servicios-grid">
-
-        <div className="servicio-card">
-          <h2>1. Desarrollo de Plataformas Digitales</h2>
-          <p>
-            Transformamos ideas en aplicaciones y plataformas digitales robustas y escalables. Desde la conceptualización hasta el lanzamiento, nuestro equipo de desarrolladores y diseñadores trabaja en estrecha colaboración para crear soluciones que optimicen procesos y mejoren la experiencia del usuario.
-          </p>
-          <ul>
-            <li><strong>Aplicaciones móviles:</strong> Diseñamos y desarrollamos aplicaciones móviles intuitivas para Android y iOS, con funcionalidades personalizadas para diferentes industrias.</li>
-            <li><strong>Plataformas web:</strong> Creamos plataformas web que permiten a las empresas gestionar sus operaciones de manera eficiente, centralizando datos y facilitando la interacción con sus usuarios.</li>
-          </ul>
-        </div>
-
-        <div className="servicio-card">
-          <h2>2. Ciberseguridad</h2>
-          <p>
-            En un entorno digital cada vez más amenazado, la ciberseguridad es esencial. UVOL ofrece servicios avanzados de ciberseguridad para proteger los datos y sistemas críticos de su organización.
-          </p>
-          <ul>
-            <li><strong>Evaluación de vulnerabilidades:</strong> Identificamos y corregimos puntos débiles en sus sistemas antes de que puedan ser explotados.</li>
-            <li><strong>Monitoreo y gestión de amenazas:</strong> Ofrecemos un monitoreo constante y la gestión de incidentes en tiempo real para asegurar la continuidad del negocio.</li>
-            <li><strong>Consultoría en ciberseguridad:</strong> Asesoramos a las organizaciones en la implementación de políticas y prácticas de seguridad robustas.</li>
-          </ul>
-        </div>
-
-        <div className="servicio-card">
-          <h2>3. Integración de Sistemas</h2>
-          <p>
-            Facilitamos la integración de sistemas y aplicaciones para que su empresa funcione como una máquina bien engrasada. Nuestros servicios de integración garantizan que todas sus herramientas y plataformas trabajen en conjunto de manera eficiente.
-          </p>
-          <ul>
-            <li><strong>Integración de CRM y ERP:</strong> Conectamos sistemas de gestión de clientes y recursos empresariales para mejorar la eficiencia operativa.</li>
-            <li><strong>Automatización de procesos:</strong> Implementamos soluciones que automatizan tareas repetitivas, reduciendo errores y liberando tiempo para actividades más estratégicas.</li>
-          </ul>
-        </div>
-
-        <div className="servicio-card">
-          <h2>4. Consultoría Tecnológica</h2>
-          <p>
-            Nuestros expertos ofrecen asesoramiento estratégico para ayudar a su empresa a aprovechar las tecnologías emergentes y mantenerse competitiva en el mercado.
-          </p>
-          <ul>
-            <li><strong>Análisis y diseño de soluciones:</strong> Evaluamos sus necesidades tecnológicas y diseñamos soluciones a medida que se alineen con sus objetivos comerciales.</li>
-            <li><strong>Transformación digital:</strong> Ayudamos a las empresas a adoptar nuevas tecnologías y a rediseñar sus procesos para adaptarse al entorno digital actual.</li>
-          </ul>
-        </div>
-
-        <div className="servicio-card">
-          <h2>5. Soporte y Mantenimiento</h2>
-          <p>
-            Ofrecemos soporte técnico y mantenimiento continuo para asegurar que sus sistemas operen sin problemas y estén siempre actualizados.
-          </p>
-          <ul>
-            <li><strong>Soporte 24/7:</strong> Nuestro equipo está disponible en todo momento para resolver cualquier problema técnico que pueda surgir.</li>
-            <li><strong>Actualizaciones y mejoras:</strong> Nos aseguramos de que sus sistemas estén siempre en la última versión y funcionando de manera óptima.</li>
-          </ul>
-        </div>
-
+        <Accordion defaultActiveKey="0" className="servicios-accordion">
+          {services.map((service, index) => (
+            <Accordion.Item eventKey={index.toString()} key={index}>
+              <Accordion.Header>{service.title}</Accordion.Header>
+              <Accordion.Body>
+                <div className="card-content">
+                  <div className="card-text">
+                    <p className="service-description">{service.description}</p>
+                    <p>{service.details}</p>
+                  </div>
+                  <div className="card-image">
+                    <img src={service.image} alt={service.title} />
+                  </div>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
       </div>
+      <Footer />
     </div>
-    <Footer/>
-    </div>
-  )
+  );
 }
 
-export default ServicesPage
+export default ServicesPage;
